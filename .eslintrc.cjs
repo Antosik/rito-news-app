6a8d26f1137ab314c21a2fad5ba4e63e958cb44f
Antosik <grig_anton96@mail.ru>
2023-04-17 10:04:51 +0300

    misc: update lint & config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,62 +1,69 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json',
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	plugins: ['svelte3', '@typescript-eslint/eslint-plugin'],
 	extends: [
-		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:prettier/recommended',
 		'plugin:import/recommended',
 		'plugin:import/typescript',
 	],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs', 'svelte.config.js', 'vite.config.js'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript'),
-		'svelte3/ignore-styles': () => true,
-		"import/resolver": {
-			typescript: {} // this loads <rootdir>/tsconfig.json to eslint
-		},
-	},
-	parserOptions: {
-		project: './tsconfig.json',
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	ignorePatterns: ['*.cjs', '.gitignore', 'svelte.config.js', 'vite.config.js'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+    'svelte3/typescript': true,
+    "svelte3/ignore-styles": ({ lang }) => !!lang,
+		"import/resolver": {
+			typescript: {} // this loads <rootdir>/tsconfig.json to eslint
+		},
 	},
 	rules: {
 		'import/order': [
 			'error',
 			{
 				pathGroups: [
-					{
-						pattern: '@sveltejs/**',
-						group: 'builtin',
-					},
-					{
-						pattern: '$app/**',
-						group: 'external',
-					},
-					{
-						pattern: '$lib/**',
-						group: 'parent',
-					},
+          {
+            pattern: '$app/**',
+            group: 'internal',
+          },
+          {
+            pattern: '$lib/!(types)**/**',
+            group: 'parent',
+          },
+          {
+            pattern: '$lib/types',
+            group: 'type',
+          },
+          {
+            pattern: '$lib/types/**',
+            group: 'type',
+          }
 				],
 				'newlines-between': 'always',
 				alphabetize: {
 					order: 'asc',
 				},
 				groups: [
-					'type',
-					'builtin', // Built-in types are first
-					'external',
-					['parent', 'sibling', 'index'],
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          ['sibling', 'index'],
 				],
 			},
 		],
+    'import/no-unresolved': [2, { ignore: ['^\\$[env|app|locales]'] }]
 	},
 };
