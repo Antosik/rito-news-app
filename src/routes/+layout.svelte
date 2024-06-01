e9d12c7824f967b9ec130bf0ce230fd5a833282e
Antosik <grig_anton96@mail.ru>
2024-06-01 13:06:48 +0300

    misc: add some metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+  import { t } from 'svelte-intl-precompile';
+
   import '../app.scss';
+  import { page } from '$app/stores';
+
+  $: key = $page.route.id === '/jobs' ? 'jobs' : 'news';
+  $: title = `rito.news - ${$t(key)}`;
+  $: description = $t('about-text');
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta property="og:title" content={title} />
+  <meta name="twitter:title" content={title} />
+  <meta property="og:url" content={$page.url.toString()} />
+
+  <meta name="description" content={description} />
+  <meta property="og:description" content={description} />
+  <meta name="twitter:description" content={description} />
+</svelte:head>
 
 <slot />
